7bb9717bd10e7b511461d77bdb70b084298e6350
US <singhu12345890@gmail.com>
2025-02-21 10:18:59 +0530

    today work

---
+++ AIML/back/http/express/express-intro.js
@@ -0,0 +1,53 @@
+//const express = require('express');
+import express from 'express';
+const app = express();
+const port = 3000;
+app.get('/',(req,res)=>{
+    res.send("<h1>Welcome to Express</h1>");
+});
+
+app.get('/api/:name/:rollno',(req,res)=>{
+    try{
+const { name, rollno} = req.params;
+res.send(`Welcome ${name} and your roll no. is: ${rollno}`)
+    } catch(error){
+        console.log(`Error is: ${error.message}`);
+    }
+})
+// app.get('/api',(req,res)=>{
+//     const {name,rollno} = req.query;
+//     res.send(Welcome to abes: ${name} and Roll no. id ${rollno});
+// });
+// http://localhost:3000/api?name=rajesh&rollno=214
+
+/*app.get('/api',(req,res)=>{
+    // const {name = "Guest",rollno = 100} = req.query;
+    const {name,rollno}= req.query;
+    if(!name){
+        return res.status(400).send("Name is required");
+    }
+    else{
+        res.send(Welcome to abes: ${name} and Roll no. id ${rollno});       
+    }
+});*/
+
+// app.get('/api',(req,res)=>{
+//     // const {name = "Guest",rollno = 100} = req.query;
+//     const {name,rollno}= req.query;
+ 
+
+//     try {
+//         if(!name){
+//             return res.status(400).send("Name is required");
+//         }
+//         else{
+//             res.send(Welcome to abes: ${name} and Roll no. id ${rollno});       
+//         }
+//     } catch (error) {
+//         console.log(Error: ${error.message})
+//     }
+// });
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
